04b3dba9062c12314be9983227223d8f9b7c8cc6
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-01-30 13:23:21 -0800

    setup single and photogrid component

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="singel-photo">
+	I am a photo grid   
+      </div>
+    )
+  }
+})
+
+export default PhotoGrid;
