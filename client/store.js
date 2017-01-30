0a7cbb5a711c15e323bc6627f9aaeec6b67eb131
Daw-Chih Liou <dawochih.liou@gmail.com>
2017-01-30 13:44:01 -0800

    add redux store

---
+++ client/store.js
@@ -0,0 +1,18 @@
+import { createStore, compse} from 'redux'
+import { syncHistoryWithStroe } from 'react-router-redux'
+import { brwoserHistory } from 'react-router'
+
+import rootReducer from './reducer/index'
+
+import comments from './data/comments'
+import posts from './data/posts'
+
+const defaultState = {
+  posts,
+  comments
+};
+
+const store = createStore(rootReducer, defaultState)
+export const history = syncHistoryWithStore(browserHistory, store)
+
+export default store;
